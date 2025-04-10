888e0a91cc74ab2218c8594e49c534f1c3c4fded
Ayoub <ayoub.ber99@gmail.com>
2025-04-10 13:39:58 +0100

    `Refactor get_driver function and update webdriver initialization`

--- scraper_avito.py
+++ scraper_avito.py
@@ -2,19 +2,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 import pandas as pd
 import time
 
-def scrape_voitures_selenium(budget, max_pages=2):
-
+def get_driver():
     options = Options()
     options.add_argument("--headless") 
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920x1080")
     options.add_argument("--no-sandbox")  
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
+def scrape_voitures_selenium(budget, max_pages=2):
 
-
-    driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+    driver = get_driver()
     voitures = []
     
     
