fe0f31f37cb87f848bd9b3c9e1766295526b179c
Ayoub <ayoub.ber99@gmail.com>
2025-04-10 13:46:12 +0100

    `Added support for Firefox driver and updated get_driver function`

--- scraper_avito.py
+++ scraper_avito.py
@@ -4,22 +4,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 import pandas as pd
 import time
 
+# Fonction pour obtenir le driver Firefox
 def get_driver():
     options = Options()
-    options.add_argument("--headless") 
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920x1080")
-    options.add_argument("--no-sandbox")  
-    return webdriver.Chrome(
-        service=Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        ),
-        options=options,
+    options.add_argument("--headless")  
+    options.add_argument("--disable-gpu")  
+
+    # Retourner un driver Firefox
+    return webdriver.Firefox(
+        service=Service(GeckoDriverManager().install()),
+        options=options
     )
+    
 def scrape_voitures_selenium(budget, max_pages=2):
 
     driver = get_driver()
